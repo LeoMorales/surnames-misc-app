c3438b213cee2a3d7bb85f7b5ff21fbc683d24d7
LeoMorales <leo.moralesr23@gmail.com>
2024-04-03 14:13:24 -0300

    adds an input

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import numpy as np
 
 st.title("¿Cuál es tu apellido?")
 
+st.text_input("Tu apellido", key="surname")
+
+input_surname = st.session_state.surname
+if input_surname:
+    st.write(f'Buscando "{input_surname}"...')
+
+
 # ¿Sabías que en el país hay 24753 personas que también se llaman como vos?
 
 # ¿Sabías que tu apellido es de origen español?
