694300ee8d36341d3137db8c59b9f27aaff35161
LeoMorales <leo.moralesr23@gmail.com>
2024-04-03 13:48:43 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("¿Cuál es tu apellido?")
+
+# ¿Sabías que en el país hay 24753 personas que también se llaman como vos?
+
+# ¿Sabías que tu apellido es de origen español?
+# ¿Sabías que la mayoría de la gente que tiene tu mismo apellido vive en Entre Rios?
